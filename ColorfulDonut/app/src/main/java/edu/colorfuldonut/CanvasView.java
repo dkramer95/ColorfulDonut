46c4420ba4a974cf4e8df84f8a588a4f5318bcca
David Kramer <daveskypilot@gmail.com>
2017-04-18 15:22:36 -0600

    Reworked the tool interface and input handling

--- ColorfulDonut/app/src/main/java/edu/colorfuldonut/CanvasView.java
+++ ColorfulDonut/app/src/main/java/edu/colorfuldonut/CanvasView.java
@@ -3,25 +3,20 @@ package edu.colorfuldonut;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
-
-import java.util.Random;
 
 /**
  * Created by DavidKramer on 4/17/17.
  */
 
 public class CanvasView extends View {
-    protected CanvasInputHandler m_inputHandler;
+    protected Tool m_currentTool;
     protected Canvas m_canvas;
     protected Bitmap m_bitmap;
     protected Paint m_paint;
-    protected float m_x;
-    protected float m_y;
 
     public CanvasView(Context c, AttributeSet attrs) {
         super(c, attrs);
@@ -30,11 +25,8 @@ public class CanvasView extends View {
 
     protected void init() {
         m_paint = new Paint();
-        m_bitmap = Bitmap.createBitmap(500, 500, Bitmap.Config.ARGB_8888);
+        m_bitmap = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
         m_canvas = new Canvas(m_bitmap);
-
-        // SAMPLE
-        m_paint.setTextSize(35);
     }
 
     public void saveImage() {
@@ -45,41 +37,19 @@ public class CanvasView extends View {
 
     }
 
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        drawDemo(canvas);
+    public Bitmap getBitmap() {
+        return m_bitmap;
     }
 
-    protected void drawDemo(Canvas canvas) {
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
         canvas.drawBitmap(m_bitmap, 0, 0, m_paint);
-        canvas.drawCircle(m_x, m_y, 100, m_paint);
-        m_paint.setColor(getRandColorInt());
-        canvas.drawText("I'm just a sample!", m_x, m_y + 150, m_paint);
-    }
-    public int getRandColorInt() {
-        Random rng = new Random();
-        int a = rng.nextInt(255);
-        int r = rng.nextInt(255);
-        int g = rng.nextInt(255);
-        int b = rng.nextInt(255);
-        int c = Color.argb(255, r, g, b);
-        return c;
     }
 
     public boolean onTouchEvent(MotionEvent e) {
-        // direct touch event to our input handler which will
-        // be one of our tools -- i.e. brush, paint bucket, etc...
+        m_currentTool.handleInput(e, this);
 
-
-        // sample touch --> TODO move to input handler
-        switch (e.getAction()) {
-            case MotionEvent.ACTION_MOVE:
-            case MotionEvent.ACTION_DOWN:
-                m_x = e.getX();
-                m_y = e.getY();
-                invalidate();
-                break;
-        }
+        // TODO what boolean should we really be returning??
         return false;
     }
 
--- ColorfulDonut/app/src/main/java/edu/colorfuldonut/Tool.java
+++ ColorfulDonut/app/src/main/java/edu/colorfuldonut/Tool.java
@@ -1,17 +1,20 @@
 package edu.colorfuldonut;
 
+import android.graphics.Paint;
+import android.view.MotionEvent;
+
 /**
  * Created by DavidKramer on 4/17/17.
  */
 
-public abstract class Tool {
+public abstract class Tool extends Paint {
     protected String m_name;
 
     public Tool(String name) {
         m_name = name;
     }
 
-    public abstract void handleInput();
+    public abstract void handleInput(MotionEvent e, CanvasView canvasView);
 
     public String getName() {
         return m_name;
